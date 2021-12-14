2ddce86e0cdc3d1ac65d7d010175c6a186c695aa
ubhargav14 <92428887+ubhargav14@users.noreply.github.com>
2021-12-14 18:10:25 +0530

    Delete amplify directory

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "SAMPLE2": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
